e8f0298a0a3d4cf7dbd4bd8fc83512ad3cf9a6bb
rvv <5573352@gmail.com>
2026-04-22 18:55:17 +0300

    feat: remove timer and hint from video note recording overlay

--- src/main/java/org/thoughtcrime/securesms/components/VideoNoteRecordingOverlay.java
+++ src/main/java/org/thoughtcrime/securesms/components/VideoNoteRecordingOverlay.java
@@ -5,20 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.TextView;
 import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.camera.view.PreviewView;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
 import org.thoughtcrime.securesms.R;
 
 public class VideoNoteRecordingOverlay extends FrameLayout {
 
   private final PreviewView previewView;
   private final VideoNoteProgressRing progressRing;
-  private final TextView timerView;
 
   public VideoNoteRecordingOverlay(@NonNull Context context) {
     super(context);
@@ -26,7 +22,6 @@ public class VideoNoteRecordingOverlay extends FrameLayout {
 
     previewView = findViewById(R.id.video_note_preview);
     progressRing = findViewById(R.id.video_note_recording_ring);
-    timerView = findViewById(R.id.video_note_timer);
 
     // Clip preview to circle
     previewView.setOutlineProvider(
@@ -58,12 +53,6 @@ public class VideoNoteRecordingOverlay extends FrameLayout {
 
   public void updateProgress(float progress) {
     progressRing.setProgress(progress);
-  }
-
-  public void updateTimer(long elapsedMs) {
-    long seconds = TimeUnit.MILLISECONDS.toSeconds(elapsedMs);
-    long tenths = (elapsedMs / 100) % 10;
-    timerView.setText(String.format(Locale.US, "%d:%02d.%d", seconds / 60, seconds % 60, tenths));
   }
 
   @MainThread
